9d8da7f3a0f6ff606884155c901fb02d19aa2379
= <valentin.weichsel@tuhh.de>
2024-12-18 15:01:52 +0100

    fixed save and load functions

--- Python_Skripts/GUI.py
+++ Python_Skripts/GUI.py
@@ -32,9 +32,8 @@ class UserInterface:
         self.root.wm_state("zoomed")
 
         self.tab_count = 0
-        #self.tabs = {}
-
-        self.data = {} # Data dictionary
+    
+        self.data = self.create_empty_data
 
         #implement support for multiple data tabs
         
@@ -81,8 +80,17 @@ class UserInterface:
         self.connect_stage() # TODO decide if necessary
         self.connect_hexapod()
 
-        # TODO decide if necessary
 
+        # TODO decide if necessary
+    def create_empty_data():
+        data = {}
+        data["Slices"] = {}
+        data["3D"] = {}
+        data["Measurements"] = {}
+        data['Info'] = {}
+
+        return data 
+    
     def create_menu(self):
         menubar = tk.Menu(self.root)
         self.root.config(menu=menubar)
@@ -802,8 +810,8 @@ class UserInterface:
         sensor_info_frame.grid_columnconfigure(1, weight=100)
 
         measurment_slider.set(1)
-        measurment_slider.config(resolution=1, state="normal")
-        measurment_slider.bind("<ButtonRelease-1>", self.update_tab)
+        measurment_slider.config(resolution=1, state="normal", command=self.update_tab)
+        #measurment_slider.bind("<ButtonRelease-1>", self.update_tab)
         #measurment_slider.bind("<Motion>", self.update_tab) TODO find correct event to update when incrementing slider
 
         sensor_readings_frame = sensor_info_frame.nametowidget("sensor_readings_frame")
@@ -935,8 +943,6 @@ class UserInterface:
         tab_name = self.tab_group.select()
         tab = self.root.nametowidget(tab_name)
 
-        self.data["Slices"] = {} 
-
         slice_slider.config(command=self.update_slice_plot)
         contour_levels_slider.config(command=self.update_slice_plot)
         interpolation_checkbox.config(command=self.update_slice_plot) 
@@ -998,8 +1004,8 @@ class UserInterface:
         tab = self.root.nametowidget(tab_name)
 
         measurement_slider = tab.nametowidget("sensor_info_frame").nametowidget("measurement_slider")
-        self.current_measurement_id = str(measurement_slider.get())# Get the current measurement id from the slider
-        
+        self.current_measurement_id = str(measurement_slider.get())
+
 
         if tab:
             self.update_measurement_info_frame(tab, self.data)
@@ -1007,9 +1013,6 @@ class UserInterface:
 
             if self.data["Slices"] != {}:
                 self.update_slice_plot()
-
-        sensor_info_frame = tab.nametowidget("sensor_info_frame")
-        sensor_info_frame.config(text="Measurement " + str(self.current_measurement_id) + "/" + str(self.measurement_points)) # Update the title
 
 
 
@@ -1086,6 +1089,10 @@ class UserInterface:
 
         current_measurement_data = data["Measurements"][self.current_measurement_id]
 
+        sensor_info_frame.config(text="Measurement " + str(self.current_measurement_id) + "/" + str(self.measurement_points)) # Update the title
+
+
+
         # Update the sensor readings
         xpos_label = sensor_readings_frame.nametowidget("xpos_label")
         ypos_label = sensor_readings_frame.nametowidget("ypos_label")
@@ -1098,22 +1105,6 @@ class UserInterface:
         xdiff_label.config(text=f"X Diff: {current_measurement_data['Signal_xdiff']:.2f}")
         ydiff_label.config(text=f"Y Diff: {current_measurement_data['Signal_ydiff']:.2f}")
         sum_label.config(text=f"Sum: {current_measurement_data['Signal_sum']:.2f}")
-
-        """ # TODO might be redundant
-        # Update the sensor position
-        sensor_position_frame = sensor_info_frame.nametowidget("sensor_position_frame")
-        sensor_position_x_label = sensor_position_frame.nametowidget("sensor_position_x_label")
-        sensor_position_y_label = sensor_position_frame.nametowidget("sensor_position_y_label")
-        sensor_position_z_label = sensor_position_frame.nametowidget("sensor_position_z_label")
-        sensor_rotation_label = sensor_position_frame.nametowidget("sensor_rotation_label")
-        sensor_distance_to_probe_label = sensor_position_frame.nametowidget("sensor_distance_to_probe_label")
-
-        sensor_position_x_label.config(text=f"X Position: {current_measurement_data['Sensor_xpos']}")
-        sensor_position_y_label.config(text=f"Y Position: {current_measurement_data['Sensor_ypos']}")
-        sensor_position_z_label.config(text=f"Z Position: {current_measurement_data['Sensor_zpos']}")
-        #sensor_rotation_label.config(text=f"Rotation: {current_measurement_data['Sensor_rotation']}")
-        # TODO calculate distance to probe and show it here
-        """
 
         # Plot the x pos and y pos in sensor_plot_frame
         sensor_plot_frame = sensor_info_frame.nametowidget("sensor_plot_frame")
@@ -1231,12 +1222,38 @@ class UserInterface:
             file_name = self.save_data(directory, self.data)
             self.log_event(f"Data saved to {file_name}")
     def load_button_pushed(self):
-        file_path = filedialog.askopenfilename(filetypes=[("htm5 files", "*.h5")])
+        file_path = filedialog.askopenfilename(filetypes=[("hdf5 files", "*.h5")])
         if file_path:
             self.data = self.load_data(file_path)
-            print(self.data)
+            #pprint.pprint(self.data)
+            #self.print_data_with_types(self.data)
             self.create_tab()
             self.update_tab()
+
+            if "Slices" in self.data:
+                print(f"Key 'Slices' exists in self.data")
+                print(f"Type of self.data['Slices']: {type(self.data['Slices'])}")
+                print(f"Content of self.data['Slices']: {self.data['Slices']}")
+                if self.data["Slices"] != {}:
+                    self.update_slice_plot()
+            else:
+                print("Key 'Slices' does not exist in self.data")
+
+
+            self.measurement_points = self.data["3D"]["measurement_points"]
+            self.current_measurement_id = 0
+
+            tab_name = self.tab_group.select()
+            tab = self.root.nametowidget(tab_name)
+
+            measurement_slider = tab.nametowidget("sensor_info_frame").nametowidget("measurement_slider")
+            measurement_slider.config(to=self.measurement_points)
+            measurement_slider.set(self.current_measurement_id+1)
+
+            slice_slider = tab.nametowidget("results_frame").nametowidget("slice_plot_frame").nametowidget("slice_slider")
+            slice_slider.config(from_=1, to=len(self.data["Slices"]))
+            
+
             self.log_event(f"Data loaded from {file_path}")
     def stop_button_pushed(self):
         self.measurement_running = False
@@ -1247,6 +1264,7 @@ class UserInterface:
     def print_data_with_types(self, data, indent=0):
         # Print the data with types
         # Fix saving issues with h5py format
+        # TODO delete this function
 
         indent_str = ' ' * indent
         if isinstance(data, dict):
@@ -1262,15 +1280,25 @@ class UserInterface:
         else:
             print(f"{indent_str}{data} (type: {type(data)})")
 
-    def convert_data(self,data):
-        if isinstance(data, dict):
-            return {key: self.convert_data(value) for key, value in data.items()}
-        elif isinstance(data, (list, tuple)):
-            return np.array(data, dtype=np.float64)  # Convert lists or tuples to numpy arrays of floats
-        elif isinstance(data, np.ndarray) and data.dtype == object:
-            return data.astype(np.float64)  # Convert object arrays to float arrays
-        else:
-            return data
+    def convert_data(self, data):
+        try:
+            if isinstance(data, dict):
+                return {key: self.convert_data(value) for key, value in data.items()}
+            elif isinstance(data, (list, tuple)):
+                return np.array(data, dtype=np.float64)  # Convert lists or tuples to numpy arrays of floats
+            elif isinstance(data, np.ndarray):
+                if data.dtype == object or data.dtype.kind in {'i', 'u'}:
+                    print(f"object array: {data}") # Convert object arrays to float arrays
+                    return data.astype(np.float64)
+                else:
+                    return data
+            else:
+                return data
+
+        except TypeError as e:
+            print(f"Error converting data: {e}")
+            print(f"Type of data: {type(data)}, dtype: {data.dtype if isinstance(data, np.ndarray) else 'N/A'}")
+            raise
     def save_data(self, data_folder, data):
 
         # Ensure the folder exists
@@ -1284,45 +1312,48 @@ class UserInterface:
         #convert data --> ensure that all data is in the correct format
         data = self.convert_data(data)
         #pprint.pprint(data)
-        self.print_data_with_types(data)
+        #self.print_data_with_types(data)
 
-        # Write data to HDF5
-        with h5py.File(hdf5_file_path, 'w') as hdf5_file:
+        # Recursive function to save nested dictionaries
+        def save_group(group, data):
             for key, value in data.items():
                 key_str = str(key)  # Convert key to string
-                if isinstance(value, (list, tuple)):
-                    value = np.array(value)  # Convert lists or tuples to numpy arrays
                 if isinstance(value, dict):
-                    if value: # Check if the dictionary is not empty
-                        group = hdf5_file.create_group(key_str)
-                        for sub_key, sub_value in value.items():
-                            sub_key_str = str(sub_key)
-                            if isinstance(sub_value, (list, tuple)):
-                                sub_value = np.array(sub_value)
-                            if sub_value is not None:
-                                group.create_dataset(sub_key_str, data=sub_value)
-                            else:
-                                print(f"Skipping None value for {sub_key_str}")
+                    if value:  # Check if the dictionary is not empty
+                        subgroup = group.create_group(key_str)
+                        save_group(subgroup, value)
                     else:
                         print(f"Skipping empty dictionary for {key_str}")
                 else:
                     if value is not None:
-                        hdf5_file.create_dataset(key_str, data=value)
+                        group.create_dataset(key_str, data=value)
                     else:
                         print(f"Skipping None value for {key_str}")
 
-        print(f"Data saved to {hdf5_file_path}") 
+
+        # Write data to HDF5
+        with h5py.File(hdf5_file_path, 'w') as hdf5_file:
+            save_group(hdf5_file, data)
+                
         return hdf5_file_path
     def load_data(self, hdf5_file_path):
+        # Recursive function to load nested dictionaries
+        def load_group(group): 
+            data = {}
+            for key in group.keys():
+                #print(f'{key} +{group[key]}')
+                item = group[key]
+                if isinstance(item, h5py.Group):
+                    data[key] = load_group(item)
+                else:
+                    data[key] = item[()]
+
+            return data
+
 
         with h5py.File(hdf5_file_path, 'r') as hdf5_file:
-            data = {}
-            for key in hdf5_file.keys():
-                group = hdf5_file[key]
-                if isinstance(group, h5py.Group):
-                    data[key] = {sub_key: group[sub_key][()] for sub_key in group.keys()}
-                else:
-                    data[key] = group[()]
+            data = load_group(hdf5_file)
+
         return data
     def add_meta_data(self, data):
         data["camera"] = {
@@ -1340,12 +1371,6 @@ class UserInterface:
             "elapsed_time": self.elapsed_time,
             "start_time": self.start_time
         }
-
-        data["beam_visualization"] = {
-
-            #TODO add beam visualization data here
-
-        }
     def add_3D_data(self, data, grid, grid_size, step_size, path):
         data["3D"] = {
             "grid": grid, # (X, Y, Z)
@@ -1357,7 +1382,9 @@ class UserInterface:
             "path": path, 
             "measurement_points": len(path)
         } 
-    
+    def add_visualization_data(self, data):
+        #TODO: add in dictionary 
+        pass
     # Measurement Handling
     def estimate_time(self):
         # TODO implement time estimation
@@ -1449,14 +1476,14 @@ class UserInterface:
 
         self.add_3D_data(self.data, self.grid, self.grid_size, self.step_size, self.path_points)
          
-        self.measurement_points = self.data["3D"]["measurement_points"]
-        self.data["Measurements"] = {} # Create a dictionary to store the measurements
-        self.data["Slices"] = {} # Create a dictionary to store the slices
-
+        
+        
         self.start_time = time.time()
         self.elapsed_time = 0
 
         self.add_meta_data(self.data)
+
+        self.measurement_points = self.data["3D"]["measurement_points"]
 
         measurement_slider = tab.nametowidget("sensor_info_frame").nametowidget("measurement_slider")
         measurement_slider.config(to=self.measurement_points)
@@ -1486,7 +1513,7 @@ class UserInterface:
             self.log_event(f"Performed measurement {i+1} / {self.measurement_points}")
 
             # update 
-            measurement_slider.set(str(i+1))
+            measurement_slider.set(str(i+1)) # TODO fix current measurement id!!!
             self.update_progress_bar(progress_bar, i+1)
             self.update_tab()
 
